e9a4d5839e198089cadbe676f36f44b3e1469892
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-11-18 19:18:06 -0800

    Add social icons in hamburger menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -26,3 +26,19 @@ document.querySelector("#hamburger").addEventListener("click", () => {
     hamburgerBot.classList.remove("-translate-y-3", "-rotate-45");
   }
 });
+
+// testing toggle
+const menu = document.querySelector("#menu");
+const menuContainer = document.querySelector("#menu-container");
+const hamburgerTop = document.querySelector("#hamburger-top");
+const hamburgerMid = document.querySelector("#hamburger-mid");
+const hamburgerBot = document.querySelector("#hamburger-bot");
+menu.style.display = "flex";
+
+menuContainer.classList.remove("opacity-0");
+menuContainer.classList.add("h-svh", "opacity-100");
+
+hamburgerTop.classList.add("translate-y-3", "rotate-45");
+hamburgerMid.classList.remove("opacity-100");
+hamburgerMid.classList.add("opacity-0");
+hamburgerBot.classList.add("-translate-y-3", "-rotate-45");
