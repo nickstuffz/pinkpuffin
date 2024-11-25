e02b00a40b577b058e5c4a1d6f791c5242e6c2fa
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-11-25 14:42:18 -0800

    Add Tailwind Prose integration with Markdown

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,9 @@
 /** @type {import('tailwindcss').Config;} */
 
-const plugin = require("tailwindcss/plugin");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    plugin(function ({ addBase, theme }) {
-      addBase({
-        h1: {
-          fontSize: theme("fontSize.3xl"),
-          fontFamily: theme("fontFamily.serif"),
-        },
-      });
-    }),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
